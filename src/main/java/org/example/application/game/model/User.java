42fff3ab380de9624de0b5c42c5b58f138c1a961
Xinlan Zhang <if21b126@technikum-wien.at>
2022-12-12 16:27:59 +0100

    User class added

--- src/main/java/org/example/application/game/model/User.java
+++ src/main/java/org/example/application/game/model/User.java
@@ -1,4 +1,33 @@
 package org.example.application.game.model;
-
+//import com.fasterxml.jackson.annotation.JsonFormat;
+//@JsonFormat(with = JosnFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTISE)
 public class User {
+
+    private String username;
+
+    private String password;
+
+    public User() { //构造函数1
+    }
+
+    public User(String username, String password) { //构造函数2
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
