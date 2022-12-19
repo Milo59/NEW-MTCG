fc0b8d0479e0509082d1b96ceeab42ffadff4c2f
Xinlan Zhang <if21b126@technikum-wien.at>
2022-12-19 18:42:11 +0800

    fix: create packages (done by "admin")
    create packages (done by "admin")

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
 import org.example.application.GameApp;
+import org.example.application.utils.DatabaseUtil;
 import org.example.server.Server;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Server server = new Server(new GameApp());
         try {
             server.start(); // enter
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            DatabaseUtil.getConnection().close();
         }
     }
 }
--- src/main/java/org/example/application/packages/repository/PackageDbRepository.java
+++ src/main/java/org/example/application/packages/repository/PackageDbRepository.java
@@ -5,27 +5,44 @@ import org.example.application.utils.DatabaseUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
+import java.util.UUID;
 
 public class PackageDbRepository implements PackageRepository {
 
     @Override
     public boolean save(List<Packages> packagesList) throws Exception {
-        Connection connection = DatabaseUtil.getConnection();
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = DatabaseUtil.getConnection();
+            connection.setAutoCommit(false);
+            String packageId = UUID.randomUUID().toString();
+            String insertPackageSql = "INSERT INTO PACKAGES(ID,NAME) VALUES(?,?)";
+            ps = connection.prepareStatement(insertPackageSql);
+            ps.setString(1, packageId);
+            ps.setString(2, packageId);
+            ps.execute();
 
-        String insertUserSql = "INSERT INTO PACKAGES(ID, NAME, DAMAGE) VALUES(?, ?, ?)";
-        try (PreparedStatement ps = connection.prepareStatement(insertUserSql)) {
-            for (Packages packages:packagesList){
+            String insertCardSql = "INSERT INTO CARD(ID, P_ID, NAME, DAMAGE) VALUES(?, ?, ?, ?)";
+            ps = connection.prepareStatement(insertCardSql);
+            for (Packages packages : packagesList) {
                 ps.setString(1, packages.getId());
-                ps.setString(2, packages.getName());
-                ps.setFloat(3, packages.getDamage());
+                ps.setString(2, packageId);
+                ps.setString(3, packages.getName());
+                ps.setFloat(4, packages.getDamage());
                 ps.execute();
             }
+            connection.commit();
         } catch (Exception e) {
+            connection.rollback();
             throw e;
+        } finally {
+            connection.setAutoCommit(true);
+            if (null != ps) {
+                ps.close();
+            }
         }
-
         return true;
     }
 }
