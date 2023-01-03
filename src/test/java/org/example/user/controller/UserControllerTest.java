94a7e1a8fdea7cb7104978d99f0f019101253ecf
Xinlan Zhang <if21b126@technikum-wien.at>
2023-01-03 18:23:09 +0800

    feat: test create users
    test create users

--- src/test/java/org/example/user/controller/UserControllerTest.java
+++ src/test/java/org/example/user/controller/UserControllerTest.java
@@ -1,4 +1,39 @@
 package org.example.user.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.application.user.UserApp;
+import org.example.application.user.repository.UserDbRepository;
+import org.example.application.user.repository.UserRepository;
+import org.example.server.dto.Request;
+import org.example.server.dto.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserControllerTest {
+    UserRepository userRepository = new UserDbRepository();
+    UserApp userApp;
+    @Test
+    void testCreateUsers() throws Exception {
+        userApp = new UserApp();
+
+        Request request = new Request();
+        request.setMethod("POST");
+        request.setPath("/users");
+
+        Map<String,String> map = new HashMap<>();
+        map.put("Username","kienboec");
+        map.put("Password","daniel");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(map);
+        request.setContent(content);
+
+        // Act
+        Response response = userApp.handle(request);
+
+        //Print test output
+        System.out.println(response.getStatus());
+        System.out.println(response.getContent());
+    }
 }
