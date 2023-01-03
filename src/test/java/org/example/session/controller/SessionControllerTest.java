3ab500d07ce6f1bca4aeb2194efaa624b9ca4260
Xinlan Zhang <if21b126@technikum-wien.at>
2023-01-03 17:03:07 +0800

    feat: test user login
    test user login

---
+++ src/test/java/org/example/session/controller/SessionControllerTest.java
@@ -0,0 +1,42 @@
+package org.example.session.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.application.sessions.SessionApp;
+import org.example.server.dto.Request;
+import org.example.server.dto.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SessionControllerTest {
+
+    SessionApp sessionApp;
+
+    @Test
+    void testLoginUsers() throws Exception {
+        sessionApp = new SessionApp();
+
+        Request request = new Request();
+        request.setMethod("POST");
+        request.setPath("/sessions");
+
+        Map<String,String> map = new HashMap<>();
+        map.put("Username","kienboec");
+        map.put("Password","daniel");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(map);
+        request.setContent(content);
+
+        // Act
+        Response response = sessionApp.handle(request);
+
+        //Print test output
+        System.out.println(response.getStatus());
+        System.out.println(response.getContent());
+        // Assert
+        /*assertEquals(StatusCode.OK.code, response.getStatus());
+        assertEquals(houses.toString(), response.getContent());
+        verify(houseRepository.findAll(), times(1));*/
+    }
+}
